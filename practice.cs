58a4fe7de05d8aada45ebf12f54f493907d90e59
momot <motraore611@yahoo.com>
2023-06-09 23:12:07 -0400

    ran dotnet new console, build, and run to test

--- practice.cs
+++ practice.cs
@@ -17,15 +17,33 @@ using System;
 
 // RECEIVING INPUT
 
+// class MainClass {
+//     public static void Main (string[] args) {
+//         // Create Vaiable to hold input
+//         string input;
+//         // Prompt user for input
+//         Console.WriteLine("What is your name>");
+//         // Receive input
+//         input = Console.ReadLine();
+//         // Print message
+//         Console.WriteLine("Hey there " + input);
+//     }
+// }
+
+////////////////////////
+// -- CONDITIONALS -- //
+
+// if statements
 class MainClass {
     public static void Main (string[] args) {
-        // Create Vaiable to hold input
-        string input;
-        // Prompt user for input
-        Console.WriteLine("What is your name>");
-        // Receive input
-        input = Console.ReadLine();
-        // Print message
-        Console.WriteLine("Hey there " + input);
+        int num = 5;
+
+        if (num > 3) {
+            Console.WriteLine("num is greater than 3");
+        } else if (num > 1) {
+            Console.WriteLine("num is greater than 1");
+        } else {
+            Console.WriteLine("num is 1 or less");
+        }
     }
 }
